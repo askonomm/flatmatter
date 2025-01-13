d7350b9638b7c34707d262586b6d253d3fe48d5e
Asko Nõmm <asko@nmm.ee>
2025-01-13 22:39:24 +0200

    Add test case for ToJson serializer

---
+++ src/serializers/to_json.test.ts
@@ -0,0 +1,12 @@
+import FlatMatter from "../flatmatter";
+import ToJson from "./to_json";
+
+test("Single-level configuration", () => {
+    const fm = new FlatMatter(
+        'a: true\nb: false\nc: 1\nd: 12.5\nf: "some string"'
+    );
+
+    const equal = "{\"a\":true,\"b\":false,\"c\":1,\"d\":12.5,\"f\":\"some string\"}";
+    
+    expect(fm.serialize(new ToJson())).toStrictEqual(equal);
+});
