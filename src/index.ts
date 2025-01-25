2ac91c7e90ad8fe98f886dd5c0b3d6c92dc4b8c9
Asko Nõmm <asko@nmm.ee>
2025-01-25 22:14:44 +0200

    Remove unused var

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import FlatMatter, {FlatMatterFn, Serializer, Matter} from "./flatmatter.ts";
+import FlatMatter, {FlatMatterFn, Serializer} from "./flatmatter.ts";
 import ToObject from "./serializers/to_object.ts";
 import ToJson from "./serializers/to_json.ts";
 
 export {
     FlatMatter,
     FlatMatterFn,
-    Matter,
     Serializer,
     ToObject,
     ToJson,
